9d2ebf3e76bc5dd82d57325d61d1b2fb3216e0ea
Bogdan Arizancu <bogdan@netzstrategen.com>
2021-04-23 18:34:27 +0200

    Added orders and subscriptions filters and progress bar.

--- src/CliCommand.php
+++ src/CliCommand.php
@@ -8,6 +8,7 @@
 namespace Netzstrategen\WP_CLI_DB_Clean_Export;
 
 use \WP_CLI;
+use \WP_CLI\Utils;
 use Ifsnop\Mysqldump\Mysqldump as IMysqldump;
 
 /**
@@ -19,43 +20,69 @@ class CliCommand extends \WP_CLI_Command {
 
   public function export() {
     global $wpdb;
-    $allowedEmails = ['@netzstrategen.com' , '@bnn.de'];
+    // Set allowed email hosts.
+    if (defined('CLEAN_EXPORT_ALLOWED_EMAILS') && CLEAN_EXPORT_ALLOWED_EMAILS) {
+      $allowedEmails = array_map('trim', explode(',', DISABLE_EXTERNAL_EMAILS_EXCEPT));
+    }
+    else {
+      $allowedEmails = ['@netzstrategen.com'];
+    }
+
+    // Set target options containing credentials to be emptied before dump.
+    if (defined('CLEAN_EXPORT_DISPOSE_OPTIONS') && CLEAN_EXPORT_DISPOSE_OPTIONS) {
+      $optionsToBlank = (array) CLEAN_EXPORT_DISPOSE_OPTIONS;
+    }
+    else {
+      $optionsToBlank = ['woocommerce_paypal_settings','woocommerce_paypal_express_settings','woocommerce_paypal_plus_settings','woocommerce_ppec_paypal_settings','wplister_paypal_email','rg_gforms_key','woothemes_helper_master_key','optimus_key','elementor_pro_license_key','searchwp_license_key','gf_zero_spam_key','wple_api_key','mbc_woogoogad_api_key','wpla_api_key','woocommerce_amazon_payments_advanced_private_key'];
+    }
+
+    // Get total number of tables for the progress bar.
+    $databaseTableCount = $wpdb->get_col($wpdb->prepare("SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '%s'", DB_NAME))[0];
+
+    // Retain user ids about to be skipped for further filters.
     $droppedUsersIds = implode(',', $wpdb->get_col(
       $wpdb->prepare("SELECT u.ID FROM {$wpdb->prefix}users u WHERE u.user_email NOT REGEXP ('%s')", implode('|', $allowedEmails)
     )));
 
     try {
-      // Add constructor config overwrites.
-      $dumpSettings = [
-        'include-tables' => [
-          "{$wpdb->prefix}users",
-          "{$wpdb->prefix}usermeta",
-        ],
-      ];
-
-      $dump = new IMysqldump('mysql:host=localhost;dbname=' . DB_NAME, DB_USER, DB_PASSWORD, $dumpSettings);
+      $dump = new IMysqldump('mysql:host=localhost;dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
 
       // Set unnecessary/sensitive data.
       $dump->setTableWheres([
-        'wp_users' => "ID NOT IN ({$droppedUsersIds})",
-        'wp_usermeta' => "user_id NOT IN ({$droppedUsersIds})",
+        "{$wpdb->prefix}users" => "ID NOT IN ({$droppedUsersIds})",
+        "{$wpdb->prefix}usermeta" => "user_id NOT IN ({$droppedUsersIds})",
+        "{$wpdb->prefix}posts" => "post_type NOT IN (\"shop_order\", \"shop_subscription\")",
+        "{$wpdb->prefix}postmeta" => "post_id NOT IN (SELECT p.ID FROM {$wpdb->prefix}posts p WHERE p.post_type IN (\"shop_order\", \"shop_subscription\"))",
+        // Ignore all sessions.
+        "{$wpdb->prefix}woocommerce_sessions" => 'session_id = 0',
       ]);
 
+      $dump->setTransformTableRowHook(function ($tableName, array $row) use ($optionsToBlank, $wpdb) {
+        if ($tableName === "{$wpdb->prefix}options" && in_array($row['meta_key'], $optionsToBlank)) {
+          $row['meta_vaue'] = '';
+        }
+        return $row;
+      });
+
+      $progress = Utils\make_progress_bar('Dumping tables: ', $databaseTableCount);
+
       // Prompt table counts.
-      $dump->setInfoHook(function ($object, $info) {
+      $dump->setInfoHook(function ($object, $info) use ($progress) {
         if ($object === 'table') {
-          WP_CLI::log($info['name'] . ' -> ' . $info['rowCount']);
+          $progress->tick();
+          // WP_CLI::log($info['name'] . ' -> ' . $info['rowCount']);
         }
       });
 
       $dump->start(ABSPATH . 'clean-export.sql');
+
+      $progress->finish();
 
       WP_CLI::success('Dump file is available at ' . ABSPATH . 'clean-export.sql');
     }
     catch (\Exception $e) {
       WP_CLI::error($e->getMessage());
     }
-
   }
 
 }
