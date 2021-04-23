6e7670985a27f6637987faffa737f235e6a7122c
Bogdan Arizancu <bogdan@netzstrategen.com>
2021-04-23 15:39:22 +0200

    Added user filters.

--- src/CliCommand.php
+++ src/CliCommand.php
@@ -18,6 +18,43 @@ use Ifsnop\Mysqldump\Mysqldump as IMysqldump;
 class CliCommand extends \WP_CLI_Command {
 
   public function export() {
+    global $wpdb;
+    $allowedEmails = ['@netzstrategen.com' , '@bnn.de'];
+    $droppedUsersIds = implode(',', $wpdb->get_col(
+      $wpdb->prepare("SELECT u.ID FROM {$wpdb->prefix}users u WHERE u.user_email NOT REGEXP ('%s')", implode('|', $allowedEmails)
+    )));
+
+    try {
+      // Add constructor config overwrites.
+      $dumpSettings = [
+        'include-tables' => [
+          "{$wpdb->prefix}users",
+          "{$wpdb->prefix}usermeta",
+        ],
+      ];
+
+      $dump = new IMysqldump('mysql:host=localhost;dbname=' . DB_NAME, DB_USER, DB_PASSWORD, $dumpSettings);
+
+      // Set unnecessary/sensitive data.
+      $dump->setTableWheres([
+        'wp_users' => "ID NOT IN ({$droppedUsersIds})",
+        'wp_usermeta' => "user_id NOT IN ({$droppedUsersIds})",
+      ]);
+
+      // Prompt table counts.
+      $dump->setInfoHook(function ($object, $info) {
+        if ($object === 'table') {
+          WP_CLI::log($info['name'] . ' -> ' . $info['rowCount']);
+        }
+      });
+
+      $dump->start(ABSPATH . 'clean-export.sql');
+
+      WP_CLI::success('Dump file is available at ' . ABSPATH . 'clean-export.sql');
+    }
+    catch (\Exception $e) {
+      WP_CLI::error($e->getMessage());
+    }
 
   }
 
